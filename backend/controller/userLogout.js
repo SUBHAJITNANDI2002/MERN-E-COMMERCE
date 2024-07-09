267e45930e8aeac44f44cae4f584aee7cdc6bdf0
SUBHAJITNANDI2002 <nandisubhajit2002@gmail.com>
2024-07-09 10:33:25 +0530

    first

---
+++ backend/controller/userLogout.js
@@ -0,0 +1,18 @@
+async function userLogout(req,res){
+    try {
+        res.clearCookies("token")
+        res.json({
+            message:"Logout successfully",
+            error:false,
+            success:true,
+            data:[]
+        })
+    } catch (err) {
+        res.json({
+            message:err.message||err,
+            error:true,
+            success:false
+        })
+    }
+}
+module.exports=userLogout
---
+++ backend/routes/index.js
@@ -0,0 +1,17 @@
+const express=require('express')
+const router=express.Router()
+
+const userSignUpController=require("../controller/userSignUp")
+const userSignInController = require('../controller/userSignIn')
+const userDetailController = require('../controller/userDetail')
+const authToken = require('../middleware/authToken')
+const userLogout = require('../controller/userLogout')
+
+router.post("/signup",userSignUpController)
+router.post("/signIn",userSignInController)
+router.get('/user-details',authToken,userDetailController)
+router.get('/userLogout',userLogout)
+
+
+
+module.exports=router
